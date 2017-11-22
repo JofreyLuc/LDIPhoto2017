616d4374bb777b26c9763d388db27141534d044d
Jofrey Luc <jyeil9@laposte.net>
2017-11-22 23:27:36 +0100

    Ajout des menus inactifs

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,14 +16,38 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+
+			/* Menus */
+			MenuBar menuBar = new MenuBar();
+
+			Menu menuFile = new Menu("Fichier");
+			MenuItem newAlbum = new MenuItem("Nouvel album...");
+			MenuItem quit = new MenuItem("Quitter");
+			menuFile.getItems().addAll(newAlbum, new SeparatorMenuItem(), quit);
+
+			Menu menuAlbum = new Menu("Album");
+			MenuItem newPage = new MenuItem("Ajouter une page");
+			MenuItem deletePage = new MenuItem("Supprimer une page");
+			MenuItem importPictures = new MenuItem("Importer des images...");
+			menuAlbum.getItems().addAll(newPage, deletePage, new SeparatorMenuItem(), importPictures);
+
+			menuBar.getMenus().addAll(menuFile, menuAlbum);
+
+			/* Comportement etrange */
+			//root.getChildren().add(menuBar);
+			root.setTop(menuBar);
+
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			/* Lance l'appli en fullscreen */
+			primaryStage.setMaximized(true);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
