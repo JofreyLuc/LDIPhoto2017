3cf88b7d9d4d97e8b59b0b06e1db400fe4a5c888
Jofrey Luc <jyeil9@laposte.net>
2018-01-04 23:32:25 +0100

    Bouton d'ajout à la page (inactif)

--- src/controler/AlbumControler.java
+++ src/controler/AlbumControler.java
@@ -11,30 +11,30 @@ import model.Page;
 import model.Picture;
 
 public class AlbumControler {
-	
+
 	int current_page;
 	ImageView current_image;
 	WindowControler windowControler;
 
 	// Public pour le moment pour les tests...
 	public Album album;
-	
+
 	public AlbumControler()
 	{
 		album = new Album();
 		current_page=1;
-		
+
 		// Pour le test, on ajoute des photos dans la page 1
 		album.getPage(1).addPicture(new Picture(new File("./resources/img.jpg").toURI().toString(),5,5));
 		album.getPage(1).addPicture(new Picture(new File("./resources/img.jpg").toURI().toString(),150,10));
-		
+
 	}
-	
-	
+
+
 	public void setPageOnPane(Pane p, int num_page)
 	{
 		current_page=num_page;
-		
+
 		Page pg = album.getPage(num_page);
 		for(Picture pi : pg.getPictures())
 		{
@@ -44,7 +44,7 @@ public class AlbumControler {
 			iv.setY(pi.y);
 			iv.setOnMousePressed(new OnClickImage(this));
 			iv.setOnMouseDragged(new OnDragImage(this));
-			
+
 		}
 	}
 
@@ -56,7 +56,7 @@ public class AlbumControler {
 
 	public void setWindowControler(WindowControler windowControler) {
 		this.windowControler = windowControler;
-		
+
 	}
 
 
@@ -68,9 +68,9 @@ public class AlbumControler {
 	public void moveCurrentImage(double x, double y) {
 		if(current_image!=null)
 		{
-		this.current_image.setX(Math.min(Math.max(0,x), windowControler.getPaneWidth()-current_image.getImage().getWidth()));
-		this.current_image.setY(Math.min(Math.max(0,y), windowControler.getPaneHeight()-current_image.getImage().getHeight()));
+			this.current_image.setX(Math.min(Math.max(0,x), windowControler.getPaneWidth()-current_image.getImage().getWidth()));
+			this.current_image.setY(Math.min(Math.max(0,y), windowControler.getPaneHeight()-current_image.getImage().getHeight()));
 		}
-		
+
 	}
 }
--- src/controler/events/OnDragImage.java
+++ src/controler/events/OnDragImage.java
@@ -7,12 +7,12 @@ import javafx.scene.input.MouseEvent;
 public class OnDragImage implements EventHandler<MouseEvent> {
 
 	AlbumControler a;
-	
+
 	public OnDragImage(AlbumControler a)
 	{
 		this.a=a;
 	}
-	
+
 	@Override
 	public void handle(MouseEvent e) {
 		this.a.moveCurrentImage(e.getX(), e.getY());
