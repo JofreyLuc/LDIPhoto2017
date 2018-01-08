eb212b0fba7825c5354a1effc120d90743ee20da
Jangoat <repiquetv@gmail.com>
2018-01-08 19:12:54 +0100

    taille fenetre

--- src/application/Main.java
+++ src/application/Main.java
@@ -26,7 +26,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,1000,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
+			primaryStage.setMinWidth(920);
+			primaryStage.setMinHeight(150);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
